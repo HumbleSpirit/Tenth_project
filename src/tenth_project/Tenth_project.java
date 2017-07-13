af7b91886f637b405988579ff546a12e7e0caed3
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-07-13 18:56:07 +0300

    Exemplar of class

--- src/tenth_project/Tenth_project.java
+++ src/tenth_project/Tenth_project.java
@@ -14,20 +14,31 @@ class Student
         String studentName;
         int studentAge;
         boolean studentSex;
+        void Print()
+        {
+            System.out.print(studentName + " ");
+            System.out.print(studentAge + " " );
+            System.out.print(studentSex + "\n");
+        }
         
     }
+
 public class Tenth_project {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         Student st = new Student();
         st.studentAge = 42;
-        //st.studentName = "Sergey";
+        st.studentName = "Sergey";
         st.studentSex = true;
-        System.out.println(st.studentName);
-        // TODO code application logic here
+        st.Print();
+        Student st1 = new Student();
+        st.studentAge = 43;
+        st.studentName = "Gennady";
+        st.studentSex = true;
+        st.Print();
+        //System.out.println(st.studentAge + " "+ st.studentName); // конкатенация c bool невозможна
+        
     }
     
 }
