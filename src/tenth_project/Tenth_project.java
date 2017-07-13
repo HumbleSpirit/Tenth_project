20804d718b47d26f4aa46fce3d9607a854f0a783
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-07-13 18:42:56 +0300

    Creating class methods

--- src/tenth_project/Tenth_project.java
+++ src/tenth_project/Tenth_project.java
@@ -9,13 +9,24 @@ package tenth_project;
  *
  * @author Boro_s99
  */
+class Student
+    {
+        String studentName;
+        int studentAge;
+        boolean studentSex;
+        
+    }
 public class Tenth_project {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Student st = new Student();
+        st.studentAge = 42;
+        //st.studentName = "Sergey";
+        st.studentSex = true;
+        System.out.println(st.studentName);
         // TODO code application logic here
     }
     
