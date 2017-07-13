4dda2072f487719f997da4c4bbf4c5ffa24552f9
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-07-13 20:51:14 +0300

    New animal class with exemplars

--- src/tenth_project/Tenth_project.java
+++ src/tenth_project/Tenth_project.java
@@ -8,20 +8,49 @@ package tenth_project;
 /**
  *
  * @author Boro_s99
+ 
  */
+    class Animal
+    {
+        String animalName;
+        int animalSpeed;
+        int animalWeight;
+    
+    Animal()
+    {
+        animalName = "Lion";
+        animalSpeed = 20;
+        animalWeight = 450;
+    }
+    Animal (String _animalName, int _animalSpeed, int _animalWeigth)
+    {
+        this.animalName = _animalName;
+        this.animalSpeed = _animalSpeed;
+        this.animalWeight = _animalWeigth;
+    }
+    void Print()
+        {
+            System.out.print(this.animalName + " ");
+            System.out.print(this.animalSpeed + " " );
+            System.out.print(this.animalWeight + "\n");
+        }
+    }
+
+
+
 class Student
     {
         String studentName;
         int studentAge;
         boolean studentSex;
 
-    Student() 
+    Student() //дефолтный конструктор экземпляра класса
     {
         studentName = "Nick";
         studentAge = 20;
         studentSex = false;
     }
-     Student(String studentName, int age, boolean sex) 
+     Student(String studentName, int age, boolean sex) //конструктор класса
     {
         this.studentName = studentName;
         this.studentAge = age;
@@ -47,7 +76,7 @@ public class Tenth_project {
 
     
     public static void main(String[] args) {
-        Student st = new Student();
+        /*Student st = new Student();
         st.studentAge = 42;
         st.studentName = "Sergey";
         st.studentSex = true;
@@ -70,14 +99,31 @@ public class Tenth_project {
         //System.out.println(st.studentAge + " "+ st.studentName); // конкатенация c bool невозможна
         
         Student students[] = new Student[2];
+        for (int i = 0; i < students.length; i++) 
+        {
+            String name;
+            int age;
+            boolean sex;
+            students[i] = new Student();
+        }
         students[0] = new Student("John", 50, true);
-        students[1] = new Student();
+        students[1] = new Student(); //значение конструктора по умолчанию
         for (int i = 0; i < students.length; i++)
         {
             students[i].Print();
             System.out.println(students[i].studentName);
+        }*/
+     
+        Animal animals[] = new Animal[5];
+        animals[0] = new Animal();
+        animals[1] = new Animal("Wolf", 20, 40);
+        animals[2] = new Animal("Fox", 25, 10);
+        animals[3] = new Animal("Hedgehog", 5, 1);
+        animals[4] = new Animal("Rabbit",4 ,1);
+        for (int i = 0; i < animals.length; i++)
+        {
+            animals[i].Print();
         }
-        
     }
     
 }
