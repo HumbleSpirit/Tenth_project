37b2b6d6c714e4a0f5ed070599085a9c9f15b779
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-07-13 20:23:19 +0300

    exemplars of class in loop

--- src/tenth_project/Tenth_project.java
+++ src/tenth_project/Tenth_project.java
@@ -14,6 +14,20 @@ class Student
         String studentName;
         int studentAge;
         boolean studentSex;
+
+    Student() 
+    {
+        studentName = "Nick";
+        studentAge = 20;
+        studentSex = false;
+    }
+     Student(String studentName, int age, boolean sex) 
+    {
+        this.studentName = studentName;
+        this.studentAge = age;
+        this.studentSex = sex;
+    }
+        
         void Print()
         {
             System.out.print(this.studentName + " ");
@@ -37,15 +51,32 @@ public class Tenth_project {
         st.studentAge = 42;
         st.studentName = "Sergey";
         st.studentSex = true;
-        st.Print(20);
+        //st.Print(20);
         st.Print();
+        
+        
         Student st1 = new Student();
-        st.studentAge = 43;
-        st.studentName = "Gennady";
-        st.studentSex = true;
-        st.Print();
-        st.Print(20);
+        st1.studentAge = 43;
+        st1.studentName = "Gennady";
+        st1.studentSex = true;
+        st1.Print();
+        
+        
+        
+        Student st3 = new Student("Nick", 21, false);
+        st3.Print();
+        //st.Print(20);
+        
         //System.out.println(st.studentAge + " "+ st.studentName); // конкатенация c bool невозможна
+        
+        Student students[] = new Student[2];
+        students[0] = new Student("John", 50, true);
+        students[1] = new Student();
+        for (int i = 0; i < students.length; i++)
+        {
+            students[i].Print();
+            System.out.println(students[i].studentName);
+        }
         
     }
     
